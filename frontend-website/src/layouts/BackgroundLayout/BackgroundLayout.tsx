9a64660c664ade7085a0b9c00b665ecce08ae104
yoshisada <ryan@ipavilion.com>
2023-03-15 06:08:48 -0700

    background blobs finished

--- frontend-website/src/layouts/BackgroundLayout/BackgroundLayout.tsx
+++ frontend-website/src/layouts/BackgroundLayout/BackgroundLayout.tsx
@@ -14,9 +14,18 @@ const BackgroundLayout: React.FC<PageLayoutProps> = ({
   return (
     <>
       <div className={styles['background__container']}>
-      {children}
+
+        <div className={styles['container']}>
+          {children}
+          <div className={styles['background__blur']}></div>
+          <img src='./blobs/1.png' className={styles['blob__1']} />
+          <img src='./blobs/2.png' className={styles['blob__2']} />
+          <img src='./blobs/3.png' className={styles['blob__3']} />
+          <img src='./blobs/4.png' className={styles['blob__4']} />
+          <img src='./blobs/5.png' className={styles['blob__5']} />
+        </div>
       </div>
-      
+
     </>
   )
 }
