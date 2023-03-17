e162fa30691de40f9324fcdcd862d42030e844f8
Ben <benjaminyeung@berkeley.edu>
2023-03-17 00:26:28 -0700

    Added balance box

---
+++ frontend-extension/src/components/BalanceBox/BalanceBox.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./BalanceBox.module.scss";
+
+export default function BalanceBox() {
+  return (
+    <div className={styles["main__container"]}>
+      <div className={styles["profile__container"]}>
+        <img className={styles["profile__image"]} src="images/punk2924.png" alt="" />
+      </div>
+
+      {/* crypto balance */}
+      <div className={styles["crypto__balance"]}>1.4665 ETH</div>
+
+      {/* USD conversion of balance */}
+      <div className={styles["usd__balance"]}>$2,527.75 USD</div>
+
+      {/* Action row buttons (pause wallet, respond to sigs, etc) */}
+      <div className={styles["action__container"]}>
+        <button className={styles["action__button"]}>Pause</button>
+        <button className={styles["action__button"]}>Transfer</button>
+        <button className={styles["action__button"]}>Approve</button>
+      </div>
+    </div>
+  );
+}
--- frontend-extension/src/pages/index.tsx
+++ frontend-extension/src/pages/index.tsx
@@ -2,9 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
 import ExtensionLayout from "@/layouts/ExtensionLayout/ExtensionLayout";
 import Header from "@/components/Header/Header";
+import BalanceBox from "@/components/BalanceBox/BalanceBox";
 
 export default function Home() {
   return (
@@ -17,6 +17,7 @@ export default function Home() {
       </Head>
       <ExtensionLayout>
         <Header />
+        <BalanceBox />
       </ExtensionLayout>
     </>
   );
