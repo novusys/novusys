4075d85f536e766fdd6f8f78369ade24039357e5
Ben <benjaminyeung@berkeley.edu>
2023-03-17 00:49:54 -0700

    Added basic balance component

--- frontend-extension/src/components/Header/Header.tsx
+++ frontend-extension/src/components/Header/Header.tsx
@@ -5,9 +5,16 @@ import { FiLogOut } from "react-icons/fi";
 export default function Header() {
   return (
     <div className={styles["main__container"]}>
+      {/* redirects to dashboard */}
       <img src="logos/novusys-leaf.png" className={styles["logo"]} alt="" />
+
+      {/* dropdown menu to change chains */}
       <div className={styles["switcher__container"]}>chain</div>
-      <FiLogOut className={styles["logout__icon"]} />
+
+      {/* locks wallet with local password set upon wallet creation */}
+      <button className={styles["logout__button"]}>
+        <FiLogOut className={styles["logout_icon"]} />
+      </button>
     </div>
   );
 }
