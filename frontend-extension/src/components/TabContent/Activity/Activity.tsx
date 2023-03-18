28a999eca6f6e795f5a31a44552eb405bce65fc3
Ben <benjaminyeung@berkeley.edu>
2023-03-17 23:15:27 -0700

    Added table cell template

--- frontend-extension/src/components/TabContent/Activity/Activity.tsx
+++ frontend-extension/src/components/TabContent/Activity/Activity.tsx
@@ -1,11 +1,134 @@
 import React from "react";
 import styles from "./Activity.module.scss";
+import { Table, User, css } from "@nextui-org/react";
+
+// Simple example. Can be updated later
+type Transaction = {
+  id: number;
+  timestamp: string;
+  // Interacted with
+  address: string;
+  // Labels the transaction (Approval All ..., Transferred ..., etc)
+  action: string;
+  // Add more options later (idea for an icon mapping)
+  type: "pause" | "approve" | "transfer" | "exchange";
+  chain: string;
+  amount: string;
+};
+type Signer = {
+  id: number;
+  address: string;
+  avatar?: string;
+  role: string;
+};
 
 export default function Activity() {
+  const columns = [
+    { name: "Transaction", uid: "action" },
+    { name: "Details", uid: "amount" },
+  ];
+
+  // Fetch signers and populate array
+  const accounts: Signer[] = [
+    {
+      id: 1,
+      avatar: "",
+      address: "",
+      role: "Guardian",
+    },
+    { id: 2, avatar: "", address: "", role: "Signer" },
+    { id: 3, avatar: "", address: "", role: "Signer" },
+    { id: 4, avatar: "", address: "", role: "Guardian" },
+    { id: 5, avatar: "", address: "", role: "Guardian" },
+    { id: 6, avatar: "", address: "", role: "Guardian" },
+    { id: 7, avatar: "", address: "", role: "Guardian" },
+    { id: 8, avatar: "", address: "", role: "Guardian" },
+  ];
+
+  const transactions: Transaction[] = [
+    {
+      id: 1,
+      timestamp: "",
+      address: "0x54ut...LR94",
+      action: "Set Approval for Token",
+      type: "approve",
+      chain: "ETH",
+      amount: "-1ETH",
+    },
+    {
+      id: 1,
+      timestamp: "",
+      address: "0x54ut...LR94",
+      action: "Set Approval for Token",
+      type: "approve",
+      chain: "ETH",
+      amount: "-1ETH",
+    },
+    {
+      id: 1,
+      timestamp: "",
+      address: "0x54ut...LR94",
+      action: "Set Approval for Token",
+      type: "approve",
+      chain: "ETH",
+      amount: "-1ETH",
+    },
+    {
+      id: 1,
+      timestamp: "",
+      address: "0x54ut...LR94",
+      action: "Set Approval for Token",
+      type: "approve",
+      chain: "ETH",
+      amount: "-1ETH",
+    },
+    {
+      id: 1,
+      timestamp: "",
+      address: "0x54ut...LR94",
+      action: "Set Approval for Token",
+      type: "approve",
+      chain: "ETH",
+      amount: "-1ETH",
+    },
+    {
+      id: 1,
+      timestamp: "",
+      address: "0x54ut...LR94",
+      action: "Set Approval for Token",
+      type: "approve",
+      chain: "ETH",
+      amount: "-1ETH",
+    },
+  ];
+
+  const renderCell = (txn: Transaction, key: React.Key) => {
+    const value = txn[key as keyof typeof txn];
+    switch (key) {
+      case "action":
+        return (
+          <User squared src={txn?.timestamp} name={value}>
+            {txn.address}
+          </User>
+        );
+      default:
+        return value;
+    }
+  };
+
   return (
-    <div className={styles["main__container"]}>
-      <p>Activity Content</p>
-      {/* Activity content */}
-    </div>
+    <Table
+      css={{
+        height: "100%",
+        width: "100%",
+        padding: "0.8rem 0.8rem 0 0.8rem",
+      }}
+      selectionMode="none"
+    >
+      <Table.Header columns={columns}>{(column) => <Table.Column key={column.uid}>{column.name}</Table.Column>}</Table.Header>
+      <Table.Body items={transactions}>
+        {(item: Transaction) => <Table.Row>{(columnKey) => <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>}</Table.Row>}
+      </Table.Body>
+    </Table>
   );
 }
--- frontend-extension/src/components/TabContent/Signers/Signers.tsx
+++ frontend-extension/src/components/TabContent/Signers/Signers.tsx
@@ -1,11 +1,71 @@
 import React from "react";
 import styles from "./Signers.module.scss";
+import { Table, User, css } from "@nextui-org/react";
+
+type Signer = {
+  id: number;
+  address: string;
+  avatar?: string;
+  role: string;
+};
 
 export default function Signers() {
+  const columns = [
+    { name: "Address", uid: "address" },
+    { name: "Role", uid: "role" },
+  ];
+
+  // Fetch signers and populate array
+  const accounts: Signer[] = [
+    {
+      id: 1,
+      avatar: "https://blogs.airdropalert.com/wp-content/uploads/2021/12/Sappy-Seal-PFP-NFTs-1003x1024.png",
+      address: "0x94c7...76F3",
+      role: "Guardian",
+    },
+    {
+      id: 2,
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwIG0oQUtISCXjhR_TxH-qfxrAGHlKBPD53A&usqp=CAU",
+      address: "0x89py...09py",
+      role: "Signer",
+    },
+    { id: 3, avatar: "", address: "0x45yi...8C32", role: "Signer" },
+    { id: 4, avatar: "", address: "0xfr54...76F3", role: "Guardian" },
+    { id: 5, avatar: "", address: "0x46t9...43L1", role: "Guardian" },
+    { id: 6, avatar: "", address: "0x94c7...76F3", role: "Guardian" },
+    { id: 7, avatar: "", address: "0x94c7...76F3", role: "Guardian" },
+    { id: 8, avatar: "", address: "0x94c7...76F3", role: "Guardian" },
+  ];
+
+  const renderCell = (account: Signer, key: React.Key) => {
+    const value = account[key as keyof typeof account];
+    switch (key) {
+      case "address":
+        return <User squared src={account?.avatar} name={value}></User>;
+      case "role":
+        return (
+          <div className={styles["role"] + " " + (value == "Guardian" ? styles["role__guardian"] : styles["role__signer"])}>
+            {value}
+          </div>
+        );
+      default:
+        return value;
+    }
+  };
+
   return (
-    <div className={styles["main__container"]}>
-      <p>Signer Content</p>
-      {/* Signer content */}
-    </div>
+    <Table
+      css={{
+        height: "100%",
+        width: "100%",
+        padding: "0.8rem 0.8rem 0 0.8rem",
+      }}
+      selectionMode="none"
+    >
+      <Table.Header columns={columns}>{(column) => <Table.Column key={column.uid}>{column.name}</Table.Column>}</Table.Header>
+      <Table.Body items={accounts}>
+        {(item: Signer) => <Table.Row>{(columnKey) => <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>}</Table.Row>}
+      </Table.Body>
+    </Table>
   );
 }
